f7127adce714bf33099dc1017bd24ec9968165a5
juanpaterninach <juan.paterninach@cecar.edu.co>
2026-04-23 19:00:28 -0500

    Update empresas.py

--- Api-ImpulsarTH/app/routers/empresas.py
+++ Api-ImpulsarTH/app/routers/empresas.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from ..dependencies import get_db
 from ..models.empresa import Empresa
 from ..schemas.empresa import EmpresaCreate, EmpresaResponse
+from fastapi import HTTPException
 
 router = APIRouter(prefix="/empresas", tags=["Empresas"])
 
@@ -24,3 +25,25 @@ def crear_empresa(empresa: EmpresaCreate, db: Session = Depends(get_db)):
 @router.get("/", response_model=list[EmpresaResponse])
 def listar_empresas(db: Session = Depends(get_db)):
     return db.query(Empresa).all()
+
+@router.patch("/{empresa_id}/estado")
+def cambiar_estado_empresa(
+    empresa_id: int,
+    estado: str,
+    db: Session = Depends(get_db)
+):
+    empresa = db.query(Empresa).filter(
+        Empresa.id == empresa_id
+    ).first()
+
+    if not empresa:
+        raise HTTPException(status_code=404, detail="Empresa no encontrada")
+
+    if estado not in ["activa", "inactiva"]:
+        raise HTTPException(status_code=400, detail="Estado inválido")
+
+    empresa.estado = estado
+
+    db.commit()
+
+    return {"mensaje": f"Empresa {estado}"}
